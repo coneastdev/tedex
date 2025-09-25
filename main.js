eaa7e0e99b8b8390f5d9fb54bfa9d75e65443433
coneastdev <coneastdev@proton.me>
2025-09-25 12:15:26 +0100

    Clean up javascript

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function genVid() {
-    //videojs.getPlayer("my-video").src({ type: 'video/mp4', src: "http://download.ted.com/talks/YossiVardi_2007-light.mp4" });
+    // select random vid from list
     vid = Math.round(Math.random() * tedTalks.length);
+
+    // load video
     videojs.getPlayer("my-video").poster(tedTalks[vid][0]);
     videojs.getPlayer("my-video").src({type: 'video/mp4', src: tedTalks[vid][1]});
     document.getElementById("title").innerText = tedTalks[vid][1].slice(30)
 }
 
+// randomly generates video so you don't get the placeholder vid every time
 window.onload = function(){
     videojs.getPlayer("my-video").ready(function(){
-        var myPlayer = this;
         genVid()
-        // un comment for auto play
-        //myPlayer.play();
     });
 };
